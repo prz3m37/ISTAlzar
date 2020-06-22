18d8794cda39a6b704d3ec28f23b552814c9a990
Przemyslaw ZIELINSKI <pzielins@lnjofi040.ist.local>
2020-06-22 23:26:13 +0200

    Changed parsing method. Example configFile added

--- AlzarBenchamark/Utils.py
+++ AlzarBenchamark/Utils.py
@@ -2,21 +2,29 @@ from time import gmtime, strftime
 import pandas as pd
 
 
+# TODO: Add del in NPT AVERAGE PY !!!!
 class Utils:
 
     def __init__(self):
         pass
 
-    # TODO: Get the same for C++ today (Monday)
+    # TODO: Get the same for C++ , for python is enough
     @staticmethod
-    def parse_config_file(config_file: str) -> dict:
+    def parse_config_file(config_file: str, parameterization: str) -> dict:
         params_container = {}
         try:
             cfg_file = open(config_file, 'r')
-            for line in cfg_file.readlines():
-                param_set = line.split('=')
-                param_name, param_value = param_set[0], param_set[1]
-                params_container[param_name] = param_value
+            for line in cfg_file:
+                if parameterization in line:
+                    for subline in cfg_file:
+                        param_set = subline.split('=')
+                        if str(param_set[0]).startswith("#Set") or str(param_set[0]).startswith("\n"):
+                            break
+                        else:
+                            param_name, param_value = param_set[0], float(param_set[1])
+                            params_container[param_name] = param_value
+                else:
+                    pass
             cfg_file.close()
             print("[INFO]: All params have been red successfully !")
         except IOError:
@@ -29,7 +37,7 @@ class Utils:
         return results_data
 
     def save_test_data(self, results_file: str, captured_time: float, records_per_buffer: int,
-                       buffers_per_acquisition: int, number_of_points: int, decimation:int) -> None:
+                       buffers_per_acquisition: int, number_of_points: int, decimation: int) -> None:
 
         current_time = self.__get_current_time()
         test_data = "[PyhCode] " + str(current_time) + " " + str(captured_time) + " " + str(records_per_buffer) \

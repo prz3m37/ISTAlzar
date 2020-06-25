2d6a210cbddf45b80188f32c265a76a7d251ef37
useme <useme@ldjofi037.ist.local>
2020-06-25 17:49:47 +0200

    AlzarBenchmark v 1.0.0

--- AlzarBenchamark/DataVisualisation.py
+++ AlzarBenchamark/DataVisualisation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from DataProcessor import DataProcessor
 
@@ -16,19 +17,29 @@ class DataVisualisation(DataProcessor):
         python_code_rpb = python_code.loc[python_code["Records_per_buffer"] == records_per_buffer]
         cpp_code_rpb = cpp_code.loc[cpp_code["Records_per_buffer"] == records_per_buffer]
 
-        x_python = python_code_rpb["percentage[%]"].values
-        y_python = python_code_rpb["Efficiency"].values
+        cpp_code_mean = cpp_code_rpb.groupby(["Records_per_buffer", "percentage[%]"], as_index=False).agg(
+            {'Efficiency': ['mean', 'std']})
 
-        x_cpp = cpp_code_rpb["percentage[%]"].values
-        y_cpp = cpp_code_rpb["Efficiency"].values
+        python_code_mean = python_code_rpb.groupby(["Records_per_buffer", "percentage[%]"], as_index=False).agg(
+            {'Efficiency': ['mean', 'std']})
 
-        fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, figsize=(15, 7))
+        x_python = python_code_mean["percentage[%]"].values
+        y_python = python_code_mean["Efficiency"]["mean"].values
+        y_python_errors = python_code_mean["Efficiency"]["std"].values
 
+        x_cpp = cpp_code_mean["percentage[%]"].values
+        y_cpp = cpp_code_mean["Efficiency"]["mean"].values
+        y_cpp_errors = cpp_code_mean["Efficiency"]["std"].values
+
+        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(15, 7), sharex=True)
         fig.suptitle("NPT Average for Cpp vs Python efficiency; decimation = " +
                      str(1) + " buffer size = 4" + " records_per_buffer = " +
                      str(records_per_buffer) + " buffers_per_acquisition = 100")
-        ax1.scatter(x_cpp, y_cpp, c="b", marker="D", label='Cpp code')
-        ax2.scatter(x_python, y_python, c="r", marker="D", label='Python code')
+
+        ax1.errorbar(x_cpp, y_cpp, yerr=y_cpp_errors, label='Cpp avg efficiency', fmt='o', c="r", capthick=2,
+                     uplims=True, lolims=True)
+        ax2.errorbar(x_python, y_python, yerr=y_python_errors, label='Python avg efficiency', fmt='o', c="b",
+                     capthick=2, uplims=True, lolims=True)
         ax1.legend(loc="upper right")
         ax2.legend(loc="upper right")
         fig.text(0.5, 0.04, 't_total/t_trigger [%]', ha='center', va='center')
@@ -44,15 +55,70 @@ class DataVisualisation(DataProcessor):
         return
 
     def plot_data(self):
+
+        results_data_file = "/home/useme/Przemek//ATS9870_Results/resultsFile_correct.txt"
+        python_code, cpp_code = self.__data_processor.pass_data_to_plot(results_data_file)
+        plt.figure(figsize=(15, 7))
+        plt.subplot(211)
+        for i in [1000, 1200, 1400, 1600, 1800, 2000, 2300, 2400, 2789, 3000]:
+            print("Plotting for: " + str(i) + " records_per_buffers")
+            cpp_code_rpb = cpp_code.loc[cpp_code["Records_per_buffer"] == i]
+            cpp_code_mean = cpp_code_rpb.groupby(["Records_per_buffer", "percentage[%]"], as_index=False).agg(
+                {'Efficiency': ['mean', 'std']})
+
+            label = str(i) + "records per buffer"
+
+            x_cpp = cpp_code_mean["percentage[%]"].values
+            y_cpp = cpp_code_mean["Efficiency"]["mean"].values
+            y_cpp_errors = cpp_code_mean["Efficiency"]["std"].values
+
+            plt.errorbar(x_cpp, y_cpp, yerr=y_cpp_errors, label=label, fmt='o', capthick=2,
+                         uplims=True, lolims=True)
+
+        plt.xticks(np.arange(0, 100, step=10))
+        plt.yticks(np.arange(0.2, 1.1, step=0.1))
+        plt.grid(True)
+        plt.legend(loc='upper right', ncol=2)
+        plt.xlabel('t_total/t_trigger [%]')
+        plt.ylabel('Efficiency')
+
+        plt.subplot(212)
+        for i in [1000, 1200, 1400, 1600, 1800, 2000, 2300, 2400, 2789, 3000]:
+            print("Plotting for: " + str(i) + " records_per_buffers")
+            python_code_rpb = python_code.loc[python_code["Records_per_buffer"] == i]
+
+            python_code_mean = python_code_rpb.groupby(["Records_per_buffer", "percentage[%]"], as_index=False).agg(
+                {'Efficiency': ['mean', 'std']})
+
+            label_cpp = str(i) + "records per buffer"
+            x_python = python_code_mean["percentage[%]"].values
+            y_python = python_code_mean["Efficiency"]["mean"].values
+            y_python_errors = python_code_mean["Efficiency"]["std"].values
+
+            plt.errorbar(x_python, y_python, yerr=y_python_errors, label=label_cpp, fmt='o', capthick=2,
+                         uplims=True, lolims=True)
+
+        plt.xticks(np.arange(0, 100, step=10))
+        plt.yticks(np.arange(0.2, 1.1, step=0.1))
+        plt.grid(True)
+        plt.legend(loc='upper right', ncol=2)
+        plt.xlabel('t_total/t_trigger [%]')
+        plt.ylabel('Efficiency')
+
+        plt.title("NPT Average for Cpp and Python efficiency for all examples")
+        plt.savefig("/home/useme/Przemek/ATS9870_Results/PlotResults/cpp_python_overall.png")
+
+
         return
 
 
 def main():
     dv = DataVisualisation()
-    for i in [1000, 1200, 1400, 1600, 1800, 2000, 2300, 2400, 2789, 3000]:
+    """ for i in [1000, 1200, 1400, 1600, 1800, 2000, 2300, 2400, 2789, 3000]:
         print("Plotting for: " + str(i) + " records_per_buffers")
-        results_data_file = "/home/useme/Przemek//ATS9870_Results/resultsFile2.txt"
-        dv.plot_test_data(results_data_file=results_data_file, records_per_buffer=i)
+        results_data_file = "/home/useme/Przemek//ATS9870_Results/resultsFile_correct.txt"
+        dv.plot_test_data(results_data_file=results_data_file, records_per_buffer=i)"""
+    dv.plot_data()
     del dv
     return
 

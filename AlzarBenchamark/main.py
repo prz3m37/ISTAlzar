31cc5f38f19ed93f8766e050ebe95a40d68ac872
Przemyslaw ZIELINSKI <pzielins@lnjofi040.ist.local>
2020-06-30 22:30:01 +0200

    Generating config file to main added

--- AlzarBenchamark/main.py
+++ AlzarBenchamark/main.py
@@ -1,3 +1,4 @@
+from Utils import Utils
 from DataProcessor import DataProcessor
 from DataVisualisation import DataVisualisation
 
@@ -6,6 +7,7 @@ from DataVisualisation import DataVisualisation
 def main():
     d_processor = DataProcessor()
     d_visual = DataVisualisation()
+    utils = Utils()
 
     results_data_file = "/home/useme/Przemek/ATS9870_Results/resultsFile_correct.txt"
     data_path, data_avg_path = None, None
@@ -13,10 +15,14 @@ def main():
     number_of_averages = 5
     parameter = ""
     values_of_parameter = [1000, 1200, 1400, 1600, 1800, 2000, 2300, 2400, 2789, 3000]
+    pts = []
+    rpb = []
+    bpa = []
+    utils.generate_config_file(post_trigger_samples=pts, records_per_buff=rpb, buff_per_acq=bpa, decimation=1)
+    # TODO: Run shell script
 
     ch_A_avg, ch_B_avg, data_avg_ch_A, data_avg_ch_B = d_processor.prepare_channel_avg_data(data_path, data_avg_path,
                                                                                             buffers_per_acq)
-
     fidelity = d_processor.compare_averages(ch_A_avg, ch_B_avg, data_avg_ch_A, data_avg_ch_B)
     if fidelity:
         cnv_signal_A, cnv_signal_B = d_processor.get_converted_signal(ch_A_avg, ch_B_avg)
